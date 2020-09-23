91bd62237a30cd4081c56175282abf1fb0ed5531
uinstinct <uinstinct@outlook.com>
2020-09-23 13:21:46 +0530

    user test passing

---
+++ core/tests/test_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Testing to create a new user with email"""
+        email = "somehting@testtest.com"
+        password = "43@!%#@41"
+        user = get_user_model().objects.create_user(
+                email=email,
+                password=password
+            )
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
